70340ed0d9b3c6ba4ccb89f9c1761dc42203f825
Grigoriy <grisha101202@yandex,ru>
2020-11-09 20:02:02 +0300

    ended 1

---
+++ src/One.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class One {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println(a>=3 && a<=8 ? "true" : "false" );
+    }
+}
