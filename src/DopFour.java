0f19907b0543e9350585471d98be9a51d2d52803
Grigoriy <grisha101202@yandex,ru>
2020-11-09 20:51:36 +0300

    ended dop 4

--- src/DopFour.java
+++ src/DopFour.java
@@ -4,6 +4,6 @@ public class DopFour {
         Scanner in = new Scanner(System.in);
         in.useLocale(Locale.US);
         double x = in.nextDouble(), y=in.nextDouble();
-        System.out.println( ? "YES" : "NO" );
+        System.out.println(y > x * x - 2 && y < -x || y > x * x - 2 && y < x ? "YES" : "NO" );
     }
 }
