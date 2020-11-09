7107087da03c3470e6ae8c516746bae179c1210c
Grigoriy <grisha101202@yandex,ru>
2020-11-09 20:05:41 +0300

    change 1

--- src/One.java
+++ src/One.java
@@ -2,7 +2,8 @@ import java.util.*;
 public class One {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
+        in.useLocale(Locale.US);
+        double a = in.nextDouble();
         System.out.println(a>=3 && a<=8 ? "true" : "false" );
     }
 }
